c7532cd3b82f2cd1e322437e5793053ade5fa0b8
Michel Romero <studentenherz@gmail.com>
2024-10-10 17:06:32 -0300

    Guard code with binary feature enabled

--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,13 @@
-use std::error::Error;
-use std::io::BufReader;
-use std::io::Write;
-use std::process::Command;
-
+#[cfg(feature = "binary")]
+mod bin_utils;
+#[cfg(feature = "binary")]
 mod checker;
-mod ipc;
+#[cfg(feature = "binary")]
 mod process;
+
+#[cfg(feature = "binary")]
+pub use bin_utils::*;
+
+mod ipc;
 mod utils;
-
-pub use checker::*;
 pub use ipc::*;
-use process::*;
-pub use utils::*;
-
-/// Judge a problem against a single test case
-///
-/// # Arguments
-///
-/// `executable_path`: Path to the executable
-/// `input_path`: Path to the input file
-/// `time_limit`: Time limit in ms
-/// `memory_limit`: Memory limit in Mb
-/// `checker`: The checker that checks for correctness
-///
-/// # Returns
-///
-/// The verdict of the judge
-pub fn judge(
-    executable_path: &str,
-    input_path: &str,
-    time_limit: u64,
-    memory_limit: u64,
-    checker: impl Checker,
-) -> Result<ProblemVerdict, Box<dyn Error>> {
-    let input = std::fs::read_to_string(input_path)?;
-
-    let mut child = Command::new(executable_path)
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::piped())
-        .spawn()?;
-
-    let pid = Pid::from_raw(child.id() as i32);
-    let mut process = Process::new(pid, time_limit, memory_limit);
-
-    if let Some(stdin) = child.stdin.as_mut() {
-        stdin.write_all(input.as_bytes())?;
-    }
-
-    wait_for(&mut process);
-
-    match process.state {
-        ProcessState::Exited(0) => {
-            let stdout = child.stdout.as_mut().unwrap();
-            let mut reader = BufReader::new(stdout);
-
-            match checker.check(&mut reader) {
-                Ok(()) => Ok(ProblemVerdict::Accepted {
-                    time: process.consumed_time_ms,
-                    memory: process.consumed_memory_mb,
-                }),
-                Err(msg) => Ok(ProblemVerdict::WrongAnswer { msg }),
-            }
-        }
-        ProcessState::Exited(_) => Ok(ProblemVerdict::RuntimeError(0)),
-        ProcessState::TimeLimitExceeded => Ok(ProblemVerdict::TimeLimitExceeded),
-        ProcessState::MemoryLimitExceeded => Ok(ProblemVerdict::MemoryLimitExceeded),
-        ProcessState::IdleLimitExceeded => Ok(ProblemVerdict::IdleLimitExceeded),
-        ProcessState::RuntimeError(sig) => Ok(ProblemVerdict::RuntimeError(sig)),
-        _ => Err("An unexpected error ocurred".into()),
-    }
-}
