20c86a4a8383207327d7325e87a282d12e488b88
Michel Romero <studentenherz@gmail.com>
2024-10-18 18:48:00 -0300

    Skip serializing none

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,8 +1,10 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
 use crate::utils::ProblemVerdict;
 
-#[derive(Debug, Serialize)]
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct JudeRequest {
     pub cmd: String,
     pub cmd_options: Option<Vec<String>>,
@@ -11,7 +13,7 @@ pub struct JudeRequest {
     pub test_dir: String,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct JudgeResponse {
     pub verdict: ProblemVerdict,
     pub test_case: u32,
